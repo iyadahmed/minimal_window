7f7ece5abc1a099146497690f92247ea67e654ba
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-30 21:07:35 +0200

    Use a bitmap for faster pixel drawing

--- nanogui.c
+++ nanogui.c
@@ -9,9 +9,17 @@
 
 #define FIXED_SIZE_WINDOW_STYLE (WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU)
 
+
+void *bitmap_memory;
+BITMAPINFO bitmap_info;
+int global_width, global_height;
+
 static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 void nano_gui_create_fixed_size_window(int width, int height) {
+    global_width = width;
+    global_height = height;
+
     // Register the window class.
     const wchar_t CLASS_NAME[] = L"NanoGUI Window Class";
 
@@ -46,6 +54,23 @@ void nano_gui_create_fixed_size_window(int width, int height) {
         return;
     }
 
+    // Allocate memory for the bitmap
+    int bytes_per_pixel = 4;
+    bitmap_memory = VirtualAlloc(0,
+                                 width * height * bytes_per_pixel,
+                                 MEM_RESERVE | MEM_COMMIT,
+                                 PAGE_READWRITE
+    );
+
+    // bitmap_info struct for StretchDIBits
+    bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
+    bitmap_info.bmiHeader.biWidth = width;
+    // Negative height makes top left as the coordinate system origin for the DrawPixel function, otherwise its bottom left
+    bitmap_info.bmiHeader.biHeight = -height;
+    bitmap_info.bmiHeader.biPlanes = 1;
+    bitmap_info.bmiHeader.biBitCount = 32;
+    bitmap_info.bmiHeader.biCompression = BI_RGB;
+
     ShowWindow(hwnd, SW_NORMAL);
 }
 
@@ -68,7 +93,13 @@ static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
             // All painting occurs here, between BeginPaint and EndPaint.
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
+            StretchDIBits(hdc,
+                          0, 0,
+                          global_width, global_height,
+                          0, 0,
+                          global_width, global_height,
+                          bitmap_memory, &bitmap_info,
+                          DIB_RGB_COLORS, SRCCOPY);
             EndPaint(hwnd, &ps);
         }
             return 0;

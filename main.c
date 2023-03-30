2d5f04306d5a06eb2d18c0dfb5025ca626800015
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-30 22:27:27 +0200

    Change example's width and height and draw some colors

--- main.c
+++ main.c
@@ -2,10 +2,16 @@
 
 
 int main() {
-    nano_gui_create_fixed_size_window(800, 600);
+    int width = 640, height = 480;
+    nano_gui_create_fixed_size_window(width, height);
 
     // Main loop
     while (nano_gui_process_events()) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                nano_gui_draw_pixel(x, y, 255, 0, 255);
+            }
+        }
     }
     return 0;
 }

cd064f8717dbe4c54afbaddfe38518348ac50c71
Iyad Ahmed <iyadahmed430@gmail.com>
2023-04-01 00:30:45 +0200

    Test out wl_display_connect

--- wayland.c
+++ wayland.c
@@ -1,1 +1,14 @@
-// TODO
+#include <stdio.h>
+#include <wayland-client.h>
+
+void nano_gui_create_fixed_size_window(int width, int height) {
+    struct wl_display *display = wl_display_connect(NULL);
+    if (!display) {
+        fprintf(stderr, "Failed to connect to Wayland display.\n");
+        return;
+    }
+    fprintf(stderr, "Connection established!\n");
+
+    // TODO: disconnect when closing window
+    wl_display_disconnect(display);
+}

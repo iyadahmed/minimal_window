a6ab76c8ee9d5445f0f28b6dd7aab34807873735
Iyad Ahmed <iyadahmed430@gmail.com>
2023-04-22 20:52:06 +0200

    Wayland: complete NanoGUI API

--- wayland.c
+++ wayland.c
@@ -1,24 +1,35 @@
-// Based on https://wayland-book.com/
+#define _POSIX_C_SOURCE 200112L
 
-#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <string.h>
-
+#include <sys/mman.h>
+#include <time.h>
+#include <unistd.h>
 #include <wayland-client.h>
+#include "xdg-shell-client-protocol.h"
 
-#include "shm.h"
-
-
-static uint32_t *pixels;
-static struct wl_display *display;
-struct wl_registry *registry;
-static int global_width, global_height;
-static struct wl_surface *surface;
-
-struct state_t {
-    struct wl_shm *shm;
-    struct wl_compositor *compositor;
+struct client_state {
+    /* Globals */
+    struct wl_display *wl_display;
+    struct wl_registry *wl_registry;
+    struct wl_shm *wl_shm;
+    struct wl_compositor *wl_compositor;
+    struct xdg_wm_base *xdg_wm_base;
+    /* Objects */
+    struct wl_surface *wl_surface;
+    struct xdg_surface *xdg_surface;
+    struct xdg_toplevel *xdg_toplevel;
 };
+
+/* Global variables */
+static struct client_state state = {0};
+static uint32_t *data;
+static int data_size;
+static int global_width, global_height;
+static struct wl_buffer *buffer;
 
 static uint32_t rgb_to_u32(uint8_t r, uint8_t g, uint8_t b) {
     // Credit: https://stackoverflow.com/a/39979191/8094047
@@ -27,85 +38,141 @@ static uint32_t rgb_to_u32(uint8_t r, uint8_t g, uint8_t b) {
 }
 
 void nano_gui_draw_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-    pixels[y * global_width + x] = rgb_to_u32(r, g, b);
+    uint32_t *pixel = (uint32_t *) data;
+    pixel += y * global_width + x;
+    *pixel = rgb_to_u32(r, g, b);
 }
 
-static void
-registry_handle_global(void *data, struct wl_registry *wl_registry,
-                       uint32_t name, const char *interface, uint32_t version) {
-    struct state_t *state = data;
-    if (strcmp(interface, wl_shm_interface.name) == 0) {
-        state->shm = wl_registry_bind(
-                wl_registry, name, &wl_shm_interface, 1);
-    } else if (strcmp(interface, wl_compositor_interface.name) == 0) {
-        state->compositor = wl_registry_bind(
-                wl_registry, name, &wl_compositor_interface, 4);
+/* Shared memory support code */
+static void randname(char *buf) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    long r = ts.tv_nsec;
+    for (int i = 0; i < 6; ++i) {
+        buf[i] = 'A' + (r & 15) + (r & 16) * 2;
+        r >>= 5;
     }
-
-    printf("interface: '%s', version: %d, name: %d\n",
-           interface, version, name);
 }
 
+static int create_shm_file(void) {
+    int retries = 100;
+    do {
+        char name[] = "/wl_shm-XXXXXX";
+        randname(name + sizeof(name) - 7);
+        --retries;
+        int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+        if (fd >= 0) {
+            shm_unlink(name);
+            return fd;
+        }
+    } while (retries > 0 && errno == EEXIST);
+    return -1;
+}
+
+static int allocate_shm_file(size_t size) {
+    int fd = create_shm_file();
+    if (fd < 0)
+        return -1;
+    int ret;
+    do {
+        ret = ftruncate(fd, size);
+    } while (ret < 0 && errno == EINTR);
+    if (ret < 0) {
+        close(fd);
+        return -1;
+    }
+    return fd;
+}
+
+/* Wayland code */
+
+static void wl_buffer_release(void *data, struct wl_buffer *wl_buffer) {
+    /* Sent by the compositor when it's no longer using this buffer */
+    wl_buffer_destroy(wl_buffer);
+}
+
+static const struct wl_buffer_listener wl_buffer_listener = {.release = wl_buffer_release,};
+
+static void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, uint32_t serial) {
+    struct client_state *state = data;
+    xdg_surface_ack_configure(xdg_surface, serial);
+
+    wl_buffer_add_listener(buffer, &wl_buffer_listener, NULL);
+    wl_surface_attach(state->wl_surface, buffer, 0, 0);
+    wl_surface_commit(state->wl_surface);
+}
+
+static const struct xdg_surface_listener xdg_surface_listener = {.configure = xdg_surface_configure,};
+
+static void xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, uint32_t serial) {
+    xdg_wm_base_pong(xdg_wm_base, serial);
+}
+
+static const struct xdg_wm_base_listener xdg_wm_base_listener = {.ping = xdg_wm_base_ping,};
+
 static void
-registry_handle_global_remove(void *data, struct wl_registry *wl_registry,
-                              uint32_t name) {
-    // This space deliberately left blank
+registry_global(void *data, struct wl_registry *wl_registry, uint32_t name, const char *interface, uint32_t version) {
+    struct client_state *state = data;
+    if (strcmp(interface, wl_shm_interface.name) == 0) {
+        state->wl_shm = wl_registry_bind(wl_registry, name, &wl_shm_interface, 1);
+    } else if (strcmp(interface, wl_compositor_interface.name) == 0) {
+        state->wl_compositor = wl_registry_bind(wl_registry, name, &wl_compositor_interface, 4);
+    } else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
+        state->xdg_wm_base = wl_registry_bind(wl_registry, name, &xdg_wm_base_interface, 1);
+        xdg_wm_base_add_listener(state->xdg_wm_base, &xdg_wm_base_listener, state);
+    }
+}
+
+static void registry_global_remove(void *data, struct wl_registry *wl_registry, uint32_t name) {
+    /* This space deliberately left blank */
 }
 
 static const struct wl_registry_listener
-        registry_listener = {
-        .global = registry_handle_global,
-        .global_remove = registry_handle_global_remove,
-};
+        wl_registry_listener = {.global = registry_global, .global_remove = registry_global_remove,};
 
 void nano_gui_create_fixed_size_window(int width, int height) {
     global_width = width;
     global_height = height;
+    state.wl_display = wl_display_connect(NULL);
+    state.wl_registry = wl_display_get_registry(state.wl_display);
+    wl_registry_add_listener(state.wl_registry, &wl_registry_listener, &state);
+    wl_display_roundtrip(state.wl_display);
 
-    display = wl_display_connect(NULL);
-    if (!display) {
-        fprintf(stderr, "Failed to connect to Wayland display.\n");
+    state.wl_surface = wl_compositor_create_surface(state.wl_compositor);
+    state.xdg_surface = xdg_wm_base_get_xdg_surface(state.xdg_wm_base, state.wl_surface);
+    xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener, &state);
+    state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
+    xdg_toplevel_set_title(state.xdg_toplevel, "Example client");
+    wl_surface_commit(state.wl_surface);
+
+
+    // Create buffer
+    int stride = width * 4;
+    data_size = stride * height;
+
+    int fd = allocate_shm_file(data_size);
+    if (fd == -1) {
         return;
     }
-    fprintf(stderr, "Connection established!\n");
 
-    registry = wl_display_get_registry(display);
-    struct state_t state = {0};
-    wl_registry_add_listener(registry, &registry_listener, &state);
+    data = mmap(NULL, data_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (data == MAP_FAILED) {
+        close(fd);
+        return;
+    }
 
-    // Do a round trip in order for `registry_handle_global` to be called and create the compositor
-    // otherwise `wl_compositor_create_surface` will fail
-    wl_display_roundtrip(display);
-
-    surface = wl_compositor_create_surface(state.compositor);
-
-    // Create shared memory pool
-    const int stride = width * 4;
-    const int shm_pool_size = height * stride * 2;
-    int fd = allocate_shm_file(shm_pool_size);
-    uint8_t *pool_data = mmap(NULL, shm_pool_size,
-                              PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    struct wl_shm_pool *pool = wl_shm_create_pool(state.shm, fd, shm_pool_size);
-
-    int index = 0;
-    int offset = height * stride * index;
-    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, offset,
-                                                         width, height, stride, WL_SHM_FORMAT_XRGB8888);
-    pixels = (uint32_t *) &pool_data[offset];
-    wl_surface_attach(surface, buffer, 0, 0);
+    struct wl_shm_pool *pool = wl_shm_create_pool(state.wl_shm, fd, data_size);
+    buffer = wl_shm_pool_create_buffer(pool, 0, width, height, stride, WL_SHM_FORMAT_XRGB8888);
+    wl_shm_pool_destroy(pool);
+    close(fd);
 }
 
 bool nano_gui_process_events() {
-    if (wl_display_dispatch(display) != -1) {
-        // Damage surface
-        // NOTE: it is not very efficient to do this every iteration, but we aim to make API minimal
-        // we could later expose an "update" function if we want to, it would be one extra function to the API.
-        wl_surface_damage(surface, 0, 0, UINT32_MAX, UINT32_MAX);
-        wl_surface_commit(surface);
+    if (wl_display_dispatch(state.wl_display)) {
         return true;
     } else {
-        wl_display_disconnect(display);
+        // Cleanup
+        munmap(data, data_size);
         return false;
     }
 }
